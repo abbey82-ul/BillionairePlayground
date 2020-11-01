a627d57181a05c71edbc1481b901920efafc0946
shauf280 <35082490+shauf280@users.noreply.github.com>
2020-11-01 14:05:51 +0000

    Signed-off-by: shauf280 <35082490+shauf280@users.noreply.github.com>

--- Website/Home.php
+++ Website/Home.php
@@ -1,1 +1,36 @@
-a
+
+<!DOCTYPE html>
+<!--[if lte IE 9]>
+<html class="ie9">
+<![endif]-->
+<!--[if gt IE 9]><!-->
+<html lang='en'>
+<!--<![endif]-->
+<head>
+
+		<meta name="author" content="Shaun Fogarty">
+		<title>Billionaire playgrounds</title>
+<link rel="stylesheet" type="text/css" href="css/header.css">
+
+</head>
+<body>
+
+
+<!-- Login Form in Navbar -->
+		<div class="topnav">
+			
+			<div class="dropdown">
+				<button class="dropbtn">Menu 
+					<i class="fa fa-caret-down"></i>
+				</button>
+				<div class="dropdown-content">
+					<a href="home.php">Home</a>
+					<a href="account.php">My Account</a>
+					
+					<a href="CurrentAuction.php">Current Auctions</a>
+					<a href="pastAuction.php">Past Auctions</a>					
+					<a href="login.php">Login</a>
+					
+				</div>
+			</div>
+</body>
